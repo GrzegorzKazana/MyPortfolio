2185347df0488e3fd6abc0c97959de8ae9a21145
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-03-25 22:43:33 +0100

    revamped hello page

--- src/components/Common/Page.jsx
+++ src/components/Common/Page.jsx
@@ -4,7 +4,7 @@ import { ArrowDownButton, ArrowUpButton } from "./Buttons/Buttons";
 
 const Page = props => (
   <div className={`Page ${props.className}`} ref={props.inputRef}>
-    <h1 className="PageTitle">{props.title}</h1>
+    {props.title && <h1 className="PageTitle">{props.title}</h1>}
     <div className="PageContent">{props.children}</div>
     {props.onScrollDown && <ArrowDownButton onClick={props.onScrollDown} />}
     {props.onScrollUp && <ArrowUpButton onClick={props.onScrollUp} />}
--- src/components/HelloPage/HelloPage.jsx
+++ src/components/HelloPage/HelloPage.jsx
@@ -3,19 +3,11 @@ import Page from "../Common/Page";
 import "./HelloPage.css";
 
 const HelloPage = ({ inputRef, ...pageProps }) => (
-  <Page title="Hello" className="HelloPage" inputRef={inputRef} {...pageProps}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur libero
-    metus, porta in nisl non, congue feugiat nisi. Morbi a elit semper mi
-    blandit tincidunt. Fusce nec venenatis lectus. Nullam aliquet vel erat at
-    semper. Vivamus a ullamcorper ipsum. Proin rhoncus tempus facilisis. Mauris
-    at metus vitae purus lobortis imperdiet vitae et felis. Morbi fringilla nibh
-    eu venenatis laoreet. Integer a lacinia lorem, eget feugiat sapien. Class
-    aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-    himenaeos. Nullam molestie urna at rhoncus consectetur. Vivamus sapien
-    metus, fringilla auctor auctor vel, semper sit amet metus. Orci varius
-    natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-    Praesent aliquet enim eu lorem fermentum, ut pellentesque turpis mollis.
-    Vivamus vestibulum odio est, eget aliquet nisl pharetra a.
+  <Page className="HelloPage" inputRef={inputRef} {...pageProps}>
+    <h1>Hi, i'm Grzesiek.</h1>
+    <h3 className="subtitle">
+      Junior Front-End Dev | ML aficionado | Eager Learner
+    </h3>
   </Page>
 );
 
