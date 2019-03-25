c0a74575759a89084e4edb2e7bc5754695ef80c3
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-03-25 23:27:34 +0100

    building skill page
    created basic layout

---
+++ src/components/SkillsPage/SkillEntry.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./SkillsPage.css";
+
+const SkillEntry = props => (
+  <div className="SkillEntry">
+    <i className="devicon-javascript-plain" />
+  </div>
+);
+export default SkillEntry;
--- src/components/SkillsPage/SkillsPage.jsx
+++ src/components/SkillsPage/SkillsPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Page from "../Common/Page";
 import "./SkillsPage.css";
+import SkillEntry from "./SkillEntry";
 
 const SkillsPage = ({ inputRef, ...pageProps }) => (
   <Page
@@ -9,18 +10,10 @@ const SkillsPage = ({ inputRef, ...pageProps }) => (
     inputRef={inputRef}
     {...pageProps}
   >
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur libero
-    metus, porta in nisl non, congue feugiat nisi. Morbi a elit semper mi
-    blandit tincidunt. Fusce nec venenatis lectus. Nullam aliquet vel erat at
-    semper. Vivamus a ullamcorper ipsum. Proin rhoncus tempus facilisis. Mauris
-    at metus vitae purus lobortis imperdiet vitae et felis. Morbi fringilla nibh
-    eu venenatis laoreet. Integer a lacinia lorem, eget feugiat sapien. Class
-    aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-    himenaeos. Nullam molestie urna at rhoncus consectetur. Vivamus sapien
-    metus, fringilla auctor auctor vel, semper sit amet metus. Orci varius
-    natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-    Praesent aliquet enim eu lorem fermentum, ut pellentesque turpis mollis.
-    Vivamus vestibulum odio est, eget aliquet nisl pharetra a.
+    <div className="SkillListBar">
+      <SkillEntry />
+    </div>
+    <div className="SkillDescription" />
   </Page>
 );
 export default SkillsPage;
