e58d4e217498975df4a05d5404d577c85901625d
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-03-26 11:58:59 +0100

    designing projects page

---
+++ src/components/ProjectsPage/ProjectEntry.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ProjectsPage.css";
+
+const ProjectEntry = props => (
+  <div className="ProjectEntry">
+    <i className="devicon-javascript-plain" />
+    <span>asdasd </span>
+  </div>
+);
+
+export default ProjectEntry;
