7778d4b95ff93f99eb6327ad842ea6d43ebcaf79
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-03-27 20:18:14 +0100

    updated text content

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -6,16 +6,16 @@ const AboutPage = ({ inputRef, ...pageProps }) => (
   <Page title="About" className="AboutPage" inputRef={inputRef} {...pageProps}>
     <div>
       <p>
-        I am a third year undergraduate student in Computer Science in Silesian
-        University Of Technology.
+        I am a third-year undergraduate student of Computer Science in The
+        Silesian University Of Technology.
       </p>
       <p>
         I am all about learning new things and broadening my current experience.
       </p>
       <p>
-        My craft is creating awesome websites with React. I'm into minimalism
-        and smart design. In my free time I also like to dive deep into the
-        world of Machine Learning. Not scared of math.
+        My craft is creating attractive websites with React. I'm into minimalism
+        and smart design. In my free time I enjoy diving deep into the world of
+        Machine Learning. Not scared of math.
       </p>
     </div>
   </Page>
