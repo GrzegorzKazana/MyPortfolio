6f74ceb88767690a5dfbd51fdfbb61c10790ad66
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-03-27 19:20:04 +0100

    Updates

---
+++ precache-manifest.fd207c98f9bb6c55cd2bfe72b8016392.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41ef2c5e8e68d7a2a057",
+    "url": "/MyPortfolio/static/js/runtime~main.ccce04bf.js"
+  },
+  {
+    "revision": "50a3823e55cbcf641577",
+    "url": "/MyPortfolio/static/js/main.d0b578bd.chunk.js"
+  },
+  {
+    "revision": "e66318fa4422e19119b6",
+    "url": "/MyPortfolio/static/js/2.3fcc083f.chunk.js"
+  },
+  {
+    "revision": "50a3823e55cbcf641577",
+    "url": "/MyPortfolio/static/css/main.29a965ba.chunk.css"
+  },
+  {
+    "revision": "87cca4eeedcef0eac9920f9b5d695124",
+    "url": "/MyPortfolio/index.html"
+  }
+];
