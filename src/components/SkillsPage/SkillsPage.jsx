8a0b52d30bc4874ad178a9942808fa7e2abc61c3
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-03 20:35:34 +0200

    using hooks in skills page instead of state

--- src/components/SkillsPage/SkillsPage.jsx
+++ src/components/SkillsPage/SkillsPage.jsx
@@ -1,41 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SkillsPage.css";
 import Page from "../Common/Page/Page";
 import SkillEntry from "./SkillEntry";
 import skillSetData from "./SkillSetData";
 
-class SkillsPage extends React.Component {
-  state = {
-    selectedSkill: 0
-  };
+const SkillsPage = ({ inputRef, ...pageProps }) => {
+  const [selectedSkill, setSkill] = useState(0);
+  return (
+    <Page
+      title="Skills"
+      className="SkillsPage"
+      inputRef={inputRef}
+      {...pageProps}
+    >
+      <div className="SkillList">
+        {skillSetData.map((skill, index) => (
+          <SkillEntry
+            key={index}
+            active={selectedSkill === index}
+            skill={skill}
+            onClick={() => setSkill(index)}
+          />
+        ))}
+      </div>
+      <div className="SkillDescription">
+        {skillSetData && skillSetData[selectedSkill].description}
+      </div>
+    </Page>
+  );
+};
 
-  onSelectSkillEntry = index => () => this.setState({ selectedSkill: index });
-
-  render() {
-    const { inputRef, ...pageProps } = this.props;
-    const { selectedSkill } = this.state;
-    return (
-      <Page
-        title="Skills"
-        className="SkillsPage"
-        inputRef={inputRef}
-        {...pageProps}
-      >
-        <div className="SkillList">
-          {skillSetData.map((skill, index) => (
-            <SkillEntry
-              key={index}
-              active={selectedSkill === index}
-              skill={skill}
-              onClick={this.onSelectSkillEntry(index)}
-            />
-          ))}
-        </div>
-        <div className="SkillDescription">
-          {skillSetData && skillSetData[selectedSkill].description}
-        </div>
-      </Page>
-    );
-  }
-}
 export default SkillsPage;
